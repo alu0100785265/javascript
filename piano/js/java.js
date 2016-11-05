f1ef45bffd20573cf5ac0243d204518bbbf144f0
alu0100785265 <alu0100785265@ull.edu.es>
2016-11-05 18:03:41 +0000

    cambios

--- piano/js/java.js
+++ piano/js/java.js
@@ -1,9 +1,13 @@
 
+
+
+//$(document).ready(function(){
+
 var piano = document.getElementById('piano');
 //piano.innerHTML += '<div class="key"></div>';
 var ruta= 'https://cdn.rawgit.com/pffy/mp3-piano-sound/master/mp3/';
 var notas = ['c1','c1s','d1','d1s','e1','f1','f1s','g1','g1s','a1','a1s','b1','c2'];
-var prueba = ['c1','c1','c1s','c1','d1s','d1','c1','c1','c1s','c1','e1','d1s','c1','c1','g1','f1','d1s','d1','c1s','f1s','f1s'];
+var prueba = ['c1','c1','d1','c1','f1','e1','c1','c1','d1','c1','g1','f1','c1','c1','c2','a1','f1','e1','d1','a1s','a1s','a1','f1','g1','f1'];
 //112143 112154 1186432 776454
 var keys = {} 
 var ki = {}
@@ -13,38 +17,26 @@ var ki = {}
    		//console.log(keys[notas[i]]);
    
      }
-  /*
-  for (j=0; j <prueba.length ; j++){
-  		ki[prueba[j]] = new Audio(ruta + prueba[j] +'.mp3');
-  		//console.log(prueba[j]);
-  }
-*/
-//
+ //var cont=0;
+ //
 
  function plays(pr){
-//	tocar();
-  //console.log(pr);
-   keys[pr].play();
-   var j=0;
-   if (notas[pr]==prueba[j]) {
-		
-   		j++;
-		console.log("correcto");
-	}
-	else{
-		console.log("error");
-	}
-   //comprobar(pr);
-}
-function comprobar(pr){
-	for(j=0; j < prueba.length; j++){
-	if (notas[pr]==prueba[j]) {
-		
+/*
+if(prueba[cont]==pr){
 
-		console.log("correcto");
-	}
-	else{
-		console.log("error");
-	}
+  //$(this).css("background","linear-gradient(-30deg,#f5f5f5,red)")
+  cont++;
+  console.log("letra correcta" + cont);
 }
+else
+  console.log("letra errónea");
+
+  */
+   console.log(pr);
+   keys[pr].play();
+ 
+   //comprobar(pr);
+
 }
+
+//});
